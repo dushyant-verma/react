285b266d2fed913d06fe93f6e46cec996a40b8aa
Dushyant Verma <167190841+dushyant-verma@users.noreply.github.com>
2024-09-06 21:17:21 +0530

    Use state, Counter, Style Tailwindcss

--- myapp/src/Hooks.js
+++ myapp/src/Hooks.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
 
 const Hooks = () => {
-
     const [state, setState] = useState(0)
+    const [value, setValue] = useState('')
 
     const increase = () => {
         setState(() => state + 2)
     }
     // create a counter using useState -- increase , decrease(native value nhi mile ) , reset
+    // When we login then all input data must be shown on console...
+    const handleInput = (e) => {
+        setValue(e.target.value)
+    }
+    // console.log(value)
+    
 
     return (
         <div>
@@ -15,6 +21,9 @@ const Hooks = () => {
             useState hooks
             <h5>{state}</h5>
             <button onClick={increase}>increase</button>
+            <h3>{value}</h3>
+
+            <input onChange={handleInput} placeholder='please enter here' />
 
         </div>
     )
--- myapp/src/components/SignupToggle.jsx
+++ myapp/src/components/SignupToggle.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function SignupToggle() {
 
     const [account, setAccount] = useState(true);
+    
 
 
     return (
